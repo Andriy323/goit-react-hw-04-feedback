5fe2955d5e511b90b5b928810c15fc9ad343e1d5
Andriy323 <108237550+Andriy323@users.noreply.github.com>
2023-02-03 21:58:59 +0200

    Update Notification.jsx

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from 'components/Notification/Notification.module.css';
+import css from './notification.module.css';
 
 export default function Notification({ message }) {
   return <p className={css.pretitleMessage}>{message}</p>;
